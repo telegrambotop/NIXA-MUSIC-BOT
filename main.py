e8c9c3509e99e113fb654fc1236a3fcc6aca9440
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-04-12 13:09:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from pytgcalls import idle
-from Zaid.main import call_py, bot, BOT
+from NIXA.main import call_py, bot, BOT
 
 async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
