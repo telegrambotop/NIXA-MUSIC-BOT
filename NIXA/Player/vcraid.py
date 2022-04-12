bcb1d05bf119b2ec89bdccd78beb3082f4323670
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-04-12 13:39:12 +0530

    Update vcraid.py

--- NIXA/Player/vcraid.py
+++ NIXA/Player/vcraid.py
@@ -1,6 +1,3 @@
-
-#forked from https://github.com/The-HellBot/ArrayCore
-
 import asyncio
 import datetime
 import logging
@@ -17,9 +14,9 @@ from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from pytgcalls.types.input_stream.quality import (HighQualityAudio, HighQualityVideo,
                                                   LowQualityVideo, MediumQualityVideo)
 
-from Zaid.queues import QUEUE, add_to_queue, get_queue, clear_queue
+from NIXA.queues import QUEUE, add_to_queue, get_queue, clear_queue
 
-from Zaid.main import call_py, bot as Zaid, Test
+from NIXA.main import call_py, bot as NIXA, Test
 from config import SUDO_USERS
 
 logging.basicConfig(level=logging.INFO)
@@ -27,19 +24,19 @@ logging.basicConfig(level=logging.INFO)
 HNDLR = '/'
 
 aud_list = [
-    "./Zaid/Audio/AUDIO1",
-    "./Zaid/Audio/AUDIO2",
-    "./Zaid/Audio/AUDIO3",
-    "./Zaid/Audio/AUDIO4",
-    "./Zaid/Audio/AUDIO5",
-    "./Zaid/Audio/AUDIO6",
-    "./Zaid/Audio/AUDIO7",
-    "./Zaid/Audio/AUDIO8",
+    "./NIXA/Audio/AUDIO1",
+    "./NIXA/Audio/AUDIO2",
+    "./NIXA/Audio/AUDIO3",
+    "./NIXA/Audio/AUDIO4",
+    "./NIXA/Audio/AUDIO5",
+    "./NIXA/Audio/AUDIO6",
+    "./NIXA/Audio/AUDIO7",
+    "./NIXA/Audio/AUDIO8",
 ]
 
 
 
-@Zaid.on_message(filters.user(SUDO_USERS) & filters.command(["vcraid"], prefixes=HNDLR))
+@NIXA.on_message(filters.user(SUDO_USERS) & filters.command(["vcraid"], prefixes=HNDLR))
 async def vcraid(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
@@ -49,23 +46,23 @@ async def vcraid(_, e: Message):
     aud = choice(aud_list) 
 
     if inp:
-        Zaid = await e.reply_text("**Starting Raid**")
-        link = f"https://zaid-robot.github.io/{aud[1:]}"
+        NIXA = await e.reply_text("**Starting Raid**")
+        link = f"https://NIXA-robot.github.io/{aud[1:]}"
         dl = aud
         songname = aud[18:]
         if chat_id in QUEUE:
             pos = add_to_queue(chat_id, songname, dl, link, "Audio", 0)
-            await Zaid.delete()
+            await NIXA.delete()
             await e.reply_text(f"**> Raiding in:** {chat.title} \n\n**> Audio:** {songname} \n**> Position:** #{pos}")
         else:
             if call_py:
                 await call_py.join_group_call(chat_id, AudioPiped(dl), stream_type=StreamType().pulse_stream)
             add_to_queue(chat_id, songname, dl, link, "Audio", 0)
-            await Zaid.delete()
+            await NIXA.delete()
             await e.reply_text(f"**> Raiding in:** {chat.title} \n\n**> Audio:** {songname} \n**> Position:** Ongoing Raid")
 
 
-@Zaid.on_message(filters.user(SUDO_USERS) & filters.command(["raidend"], prefixes=HNDLR))
+@NIXA.on_message(filters.user(SUDO_USERS) & filters.command(["raidend"], prefixes=HNDLR))
 async def ping(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
@@ -86,7 +83,7 @@ async def ping(_, e: Message):
         await e.reply_text("**No ongoing raid!**")
 
 
-@Zaid.on_message(filters.user(SUDO_USERS) & filters.command(["raidpause"], prefixes=HNDLR))
+@NIXA.on_message(filters.user(SUDO_USERS) & filters.command(["raidpause"], prefixes=HNDLR))
 async def ping(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
@@ -107,7 +104,7 @@ async def ping(_, e: Message):
         await e.reply_text("**No ongoing raid!**")
 
 
-@Zaid.on_message(filters.user(SUDO_USERS) & filters.command(["raidresume"], prefixes=HNDLR))
+@NIXA.on_message(filters.user(SUDO_USERS) & filters.command(["raidresume"], prefixes=HNDLR))
 async def ping(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
